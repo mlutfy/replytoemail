37f749d5aa2f143853fc15fcdae8927afcc18294
Mathieu Lu <mathieu@symbiotic.coop>
2025-06-09 12:03:01 -0400

    Upgrade civix, move getUsersByRole out of the civix file

--- replytoemail.php
+++ replytoemail.php
@@ -15,15 +15,6 @@ function replytoemail_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function replytoemail_civicrm_xmlMenu(&$files) {
-  _replytoemail_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -105,7 +96,7 @@ function replytoemail_civicrm_install() {
       }
 
       if (!empty($dashboard['id'])) {
-        $cids = E::getUsersByRole("client administrator");
+        $cids = replytoemail_getUsersByRole("client administrator");
         if (!empty($cids)) {
           foreach ($cids as $cid) {
             // Only add to dashboard if not already added.
@@ -130,114 +121,12 @@ function replytoemail_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function replytoemail_civicrm_postInstall() {
-  _replytoemail_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function replytoemail_civicrm_uninstall() {
-  _replytoemail_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function replytoemail_civicrm_enable() {
   _replytoemail_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function replytoemail_civicrm_disable() {
-  _replytoemail_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function replytoemail_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _replytoemail_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function replytoemail_civicrm_managed(&$entities) {
-  _replytoemail_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function replytoemail_civicrm_caseTypes(&$caseTypes) {
-  _replytoemail_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function replytoemail_civicrm_angularModules(&$angularModules) {
-  _replytoemail_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function replytoemail_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _replytoemail_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function replytoemail_civicrm_entityTypes(&$entityTypes) {
-  _replytoemail_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function replytoemail_civicrm_themes(&$themes) {
-  _replytoemail_civix_civicrm_themes($themes);
 }
 
 /**
@@ -360,6 +249,37 @@ function replytoemail_civicrm_alterReportVar($varType, &$var, $reportForm) {
       }
     }
   }
+}
+
+/**
+ * Helper function for the installer
+ * Only works on Drupal7
+ */
+function replytoemail_getUsersByRole($role) {
+  if (!function_exists('db_select')) {
+    return;
+  }
+
+  $query = db_select('users', 'u');
+  $query->fields('u', array('uid'));
+  $query->innerJoin('users_roles', 'ur', 'ur.uid = u.uid');
+  $query->innerJoin('role', 'r', 'r.rid = ur.rid');
+  $query->condition('r.name', $role);
+
+  $result = $query->execute();
+
+  $users = [];
+  foreach ($result as $user) {
+    $contact = civicrm_api3('UFMatch', 'get', [
+      'sequential' => 1,
+      'return' => ["contact_id"],
+      'uf_id' => $user->uid,
+    ]);
+    if (!empty($contact['values'][0]['contact_id'])) {
+      $users[] = $contact['values'][0]['contact_id'];
+    }
+  }
+  return $users;
 }
 
 /**
